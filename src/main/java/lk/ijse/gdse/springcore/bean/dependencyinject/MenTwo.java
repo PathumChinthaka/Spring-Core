14d91d5542cf06e371ee5bf784a7f2c52dd9defe
pathum-chinthaka <104442327+pathum-chinthaka@users.noreply.github.com>
2023-08-21 19:42:17 +0530

    constructor through DI

---
+++ src/main/java/lk/ijse/gdse/springcore/bean/dependencyinject/MenTwo.java
@@ -0,0 +1,22 @@
+package lk.ijse.gdse.springcore.bean.dependencyinject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class MenTwo {
+
+    GoodWomen women;
+
+    @Autowired
+    //Constructor through dependency injection
+    public MenTwo(GoodWomen w){
+        this.women=w;
+        System.out.println("Men Two Instantiate");
+    }
+
+    public void chatWithWomen(){
+        women.chat();
+    }
+}
